8f80f3d80ccdba69231cfb4c40ffbd8f7fb037d3
Mary Kate Tyrrell <mary.tyrrell@ucdconnect.ie>
2018-02-27 09:59:06 +0000

    reading the url input file

--- src/Assignment/Assignment3.py
+++ src/Assignment/Assignment3.py
@@ -6,14 +6,16 @@ Created on 26 Feb 2018
 '''
 
 import re
+
 pat = re.compile(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
 #practice parsing the file. 
 m = pat.match("turn on 247,321 through 1053,1053")
-print(m.group())
-print(m.group(1))
-print(m.group(2))
-print(m.group(3))
-print(m.group(4))
-print(m.group(5))
+command = m.group()
+command = m.group(1)
+x1 = m.group(2)
+x2 = m.group(3)
+y1 = m.group(4)
+y2 = m.group(5)
 
+print(command, x1, x2, y1, y2)
 
---
+++ src/Assignment/urlRequest.py
@@ -0,0 +1,11 @@
+'''
+Created on 27 Feb 2018
+
+@author: mary-katetyrrell
+'''
+import urllib
+
+
+online = urllib.urlopen("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")
+for line in online:
+    print(line)
