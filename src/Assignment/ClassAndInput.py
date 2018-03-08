b4df7d61a387ab938923c8dadc028b89d39fb52b
Mary Kate Tyrrell <mary.tyrrell@ucdconnect.ie>
2018-03-08 15:03:18 +0000

    Adjusted the coordinates to stay within the boundaries of size
    got the following error on input d:
    'NoneType' object has no attribute 'group'
    
    realised that this was to do with the regex finding a pattern. -
    included an if statement to combat this.

--- src/Assignment/ClassAndInput.py
+++ src/Assignment/ClassAndInput.py
@@ -30,13 +30,25 @@ class lightTest():
         
     def command(self, c, x1, y1, x2, y2):
         
+        #Need to make sure that the coordinates remain within the boundaries of size
+        #Making the cordinates equal to size if they are beyond it.
+        #found due to error in input file.
+        
+        if x1 > self.size:
+            x1 = self.size
+        if x2 > self.size:
+            x2 = self.size
+        if y1 > self.size:
+            y1 = self.size
+        if y2 > self.size:
+            y2 = self.size
+        
         '''Take in the parsed variables and use them accordingly'''
         
         '''check the nature of the commands and adjust the coordinates as so
         turn on - turn those coordinates to true
         turn off - turn those coordinates to false
         switch - turn the coordinates to true and the others off'''
-        
         
         if c == "turn on" or c == "turn on ":
             for i in range(y1, y2+1):
@@ -69,28 +81,33 @@ class lightTest():
         
 def parseFile(file): 
     
+    #checking if the input added starts with http
+    #if it does, use requests.
+    
     if file.startswith('http'):
         file1 = requests.get(file)
         file1 = file1.text.split('\n')
-        
         
         for line in file1[:1]:
             print("the size is", line)
             length = int(line)
             switchboard = lightTest(length)
-        for line in file1[1:-1]:
-            m = pat.match(line)
-            command = m.group(1)
-            x1 = m.group(2)
-            y1 = m.group(3)
-            x2 = m.group(4)
-            y2 = m.group(5)
-            switchboard.command(command, int(x1), int(y1), int(x2), int(y2))
+            for line in file1[1:-1]:
+            
+                find = pat.match(line)
+                if find:
+                    command = find.group(1)
+                    x1 = find.group(2)
+                    y1 = find.group(3)
+                    x2 = find.group(4)
+                    y2 = find.group(5)
+                    switchboard.command(command, int(x1), int(y1), int(x2), int(y2))
         print(switchboard.count())
         
     else:
-    #import sys
-    #file = sys.argv[2]
+        
+        #else, use the local files.
+   
         file1  = open(file, 'r')
         file1 = file1.readlines()      
         for line in file1[:1]:
@@ -98,13 +115,14 @@ def parseFile(file):
             length = int(line)
             switchboard = lightTest(length)
             for line in file1[1:]:
-                m = pat.match(line)
-                command = m.group(1)
-                x1 = m.group(2)
-                y1 = m.group(3)
-                x2 = m.group(4)
-                y2 = m.group(5)
-                switchboard.command(command, int(x1), int(y1), int(x2), int(y2))
+                find = pat.match(line)
+                if find:
+                    command = find.group(1)
+                    x1 = find.group(2)
+                    y1 = find.group(3)
+                    x2 = find.group(4)
+                    y2 = find.group(5)
+                    switchboard.command(command, int(x1), int(y1), int(x2), int(y2))
         print(switchboard.count())
      
      
